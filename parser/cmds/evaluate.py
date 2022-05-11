11aa294ece8695496122dd1d32a56e2f25b8599b
Jinwook <jinwookpark2296@gmail.com>
2022-05-11 22:13:20 +0900

    checkpoint re-setup

--- parser/cmds/evaluate.py
+++ parser/cmds/evaluate.py
@@ -17,14 +17,16 @@ from torch.utils.tensorboard import SummaryWriter
 
 class Evaluate(CMD):
 
-    def __call__(self, args, eval_dep=False, decode_type='mbr'):
+    def __call__(self, args, eval_dep=False, decode_type='mbr', data_split='test'):
         super(Evaluate, self).__call__(args)
         self.device = args.device
         self.args = args
         dataset = DataModule(args)
         self.model = get_model(args.model, dataset)
+
         best_model_path = self.args.load_from_dir + "/best.pt"
-        self.model.load_state_dict(torch.load(str(best_model_path), map_location=self.device))
+        checkpoint = torch.load(best_model_path, map_location=self.device)
+        self.model.load_state_dict(checkpoint['model'])
         print('successfully load')
 
         eval_depth = self.args.test.eval_depth if hasattr(self.args.test, 'eval_depth') else False
@@ -32,7 +34,13 @@ class Evaluate(CMD):
         right_binarization = self.args.test.right_binarization if hasattr(self.args.test, 'right_binarization') else False
         self.writer = SummaryWriter(self.args.load_from_dir)
 
-        test_loader = dataset.test_dataloader
+        if data_split == 'train':
+            test_loader = dataset.train_dataloader()
+        elif data_split == 'valid':
+            test_loader = dataset.val_dataloader
+        elif data_split == 'test':
+            test_loader = dataset.test_dataloader
+            
         test_loader_autodevice = DataPrefetcher(test_loader, device=self.device)
 
         metric_f1, metric_uas, likelihood, metric_left, metric_right = self.evaluate(
@@ -48,35 +56,35 @@ class Evaluate(CMD):
 
         # Log - Tensorboard
         for i, pf in enumerate(self.pf_sum):
-            self.writer.add_scalar('test/partition_number', pf/metric_f1.n, i)
+            self.writer.add_scalar(f'test/{data_split}/partition_number', pf/metric_f1.n, i)
         for k, v in metric_f1.sentence_uf1_d.items():
-            self.writer.add_scalar('test/f1_depth', v, k)
+            self.writer.add_scalar(f'test/{data_split}/f1_depth', v, k)
         # for k, v in metric_f1.sentence_uf1_l.items():
         #     self.writer.add_scalar('test/f1_length', v, k)
         for k, v in metric_f1.sentence_ex_d.items():
-            self.writer.add_scalar('test/Ex_depth', v, k)
+            self.writer.add_scalar(f'test/{data_split}/Ex_depth', v, k)
 
         # F1 score for each depth
         for k, v in metric_left.sentence_uf1_d.items():
-            self.writer.add_scalar('test/f1_left_depth', v, k)
+            self.writer.add_scalar(f'test/{data_split}/f1_left_depth', v, k)
         # F1 score for each length
         # for k, v in metric_left.sentence_uf1_l.items():
         #     self.writer.add_scalar('valid/f1_left_length', v, k)
         for k, v in metric_left.sentence_ex_d.items():
-            self.writer.add_scalar('test/Ex_left_depth', v, k)
+            self.writer.add_scalar(f'test/{data_split}/Ex_left_depth', v, k)
 
         # F1 score for each depth
         for k, v in metric_right.sentence_uf1_d.items():
-            self.writer.add_scalar('test/f1_right_depth', v, k)
+            self.writer.add_scalar(f'test/{data_split}/f1_right_depth', v, k)
         # F1 score for each length
         # for k, v in metric_right.sentence_uf1_l.items():
         #     self.writer.add_scalar('valid/f1_right_length', v, k)
         for k, v in metric_right.sentence_ex_d.items():
-            self.writer.add_scalar('test/Ex_right_depth', v, k)
+            self.writer.add_scalar(f'test/{data_split}/Ex_right_depth', v, k)
 
         self.estimated_depth = dict(sorted(self.estimated_depth.items()))
         for k, v in self.estimated_depth.items():
-            self.writer.add_scalar('test/estimated_depth', v/metric_f1.n, k)
+            self.writer.add_scalar(f'test/{data_split}/estimated_depth', v/metric_f1.n, k)
 
         self.estimated_depth_by_length = dict(sorted(self.estimated_depth_by_length.items()))
         for k in self.estimated_depth_by_length.keys():
@@ -84,7 +92,7 @@ class Evaluate(CMD):
         for k, v in self.estimated_depth_by_length.items():
             total = sum(v.values())
             for d, n in v.items():
-                self.writer.add_scalar(f'test/predicted_depth_by_length_{k}', n/total, d)
+                self.writer.add_scalar(f'test/{data_split}/predicted_depth_by_length_{k}', n/total, d)
 
         self.writer.flush()
         self.writer.close()
--- parser/cmds/train.py
+++ parser/cmds/train.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime, timedelta
 from parser.cmds.cmd import CMD
-from parser.helper.metric import Metric
+from parser.helper.metric import LikelihoodMetric, Metric
 from parser.helper.loader_wrapper import DataPrefetcher
 import torch
 import numpy as np
@@ -22,10 +22,16 @@ class Train(CMD):
         self.device = args.device
 
         dataset = DataModule(args)
+        self.idx2word = np.array(list(dataset.word_vocab.idx2word.values())) # for word_vocab
         self.model = get_model(args.model, dataset)
+        
+        start_epoch = 1
         if hasattr(args, 'pretrained_model'):
             with open(args.pretrained_model, 'rb') as f:
-                self.model.load_state_dict(torch.load(f))
+                checkpoint = torch.load(f)
+                start_epoch = checkpoint['epoch'] + 1
+                self.model.load_state_dict(checkpoint['model'])
+
         create_save_path(args)
         log = get_logger(args)
         self.optimizer = get_optimizer(args.optimizer, self.model)
@@ -54,8 +60,9 @@ class Train(CMD):
         self.iter = 0
         self.pf = []
         self.partition = False
+        self.total_loss = 0
 
-        for epoch in range(1, train_arg.max_epoch + 1):
+        for epoch in range(start_epoch, train_arg.max_epoch + 1):
             '''
             Auto .to(self.device)
             '''
@@ -65,11 +72,17 @@ class Train(CMD):
                 if epoch > self.train_arg.warmup_epoch:
                     self.partition = True
 
+            # partition switch for each epoch
+            self.writer.add_scalar('train/norm_switch', 1 if self.partition else 0, epoch)
+
             # curriculum learning. Used in compound PCFG.
             if train_arg.curriculum:
-                train_loader = dataset.train_dataloader(max_len=min(train_arg.start_len + epoch - 1, train_arg.max_len))
+                self.max_len = min(train_arg.start_len + epoch - 1, train_arg.max_len)
+                self.min_len = train_arg.min_len
             else:
-                train_loader = dataset.train_dataloader(max_len=train_arg.max_len)
+                self.max_len = train_arg.max_len
+                self.min_len = train_arg.min_len
+            train_loader = dataset.train_dataloader(max_len=self.max_len, min_len=self.min_len)
 
             # print_depth = self.model.depth if hasattr(self.model, 'depth') or self.model.depth == 0 else 'Not estimate.'
             # log.info(f'GIL Depth: {print_depth}')
@@ -82,9 +95,10 @@ class Train(CMD):
 
 
             dev_f1_metric, _, dev_ll, dev_left_metric, dev_right_metric = self.evaluate(eval_loader_autodevice,
-                eval_depth=eval_depth, left_binarization=left_binarization, right_binarization=right_binarization)
+                decode_type=args.test.decode, eval_depth=eval_depth, left_binarization=left_binarization, right_binarization=right_binarization)
             log.info(f"{'dev f1:':6}   {dev_f1_metric}")
             log.info(f"{'dev ll:':6}   {dev_ll}")
+
             # F1 score for each epoch
             self.writer.add_scalar('valid/F1', dev_f1_metric.sentence_uf1, epoch)
             self.writer.add_scalar('valid/F1_left', dev_left_metric.sentence_uf1, epoch)
@@ -94,33 +108,41 @@ class Train(CMD):
             self.writer.add_scalar('valid/Exact_right', dev_right_metric.sentence_ex, epoch)
             # partition function distribution
             for i, pf in enumerate(self.pf_sum):
-                self.writer.add_scalar('valid/partition_function', pf/dev_f1_metric.n, i)
-            # F1 score for each depth
-            for k, v in dev_f1_metric.sentence_uf1_d.items():
-                self.writer.add_scalar('valid/f1_depth', v, k)
+                self.writer.add_scalar(f'valid/marginal_{self.model.mode}', pf/dev_f1_metric.n, i)
+            # # F1 score for each depth
+            # for k, v in dev_f1_metric.sentence_uf1_d.items():
+            #     self.writer.add_scalar('valid/f1_depth', v, k)
             # F1 score for each length
-            # for k, v in dev_f1_metric.sentence_uf1_l.items():
-            #     self.writer.add_scalar('valid/f1_length', v, k)
-            for k, v in dev_f1_metric.sentence_ex_d.items():
-                self.writer.add_scalar('valid/Ex_depth', v, k)
-                
-            # F1 score for each depth
-            for k, v in dev_left_metric.sentence_uf1_d.items():
-                self.writer.add_scalar('valid/f1_left_depth', v, k)
-            # F1 score for each length
-            # for k, v in dev_left_metric.sentence_uf1_l.items():
-            #     self.writer.add_scalar('valid/f1_left_length', v, k)
-            for k, v in dev_left_metric.sentence_ex_d.items():
-                self.writer.add_scalar('valid/Ex_left_depth', v, k)
+            for k, v in dev_f1_metric.sentence_uf1_l.items():
+                self.writer.add_scalar('valid/f1_length', v, k)
 
-            # F1 score for each depth
-            for k, v in dev_right_metric.sentence_uf1_d.items():
-                self.writer.add_scalar('valid/f1_right_depth', v, k)
-            # F1 score for each length
-            # for k, v in dev_right_metric.sentence_uf1_l.items():
-            #     self.writer.add_scalar('valid/f1_right_length', v, k)
-            for k, v in dev_right_metric.sentence_ex_d.items():
-                self.writer.add_scalar('valid/Ex_right_depth', v, k)
+            # # Exact for each depth, length
+            # for k, v in dev_f1_metric.sentence_ex_d.items():
+            #     self.writer.add_scalar('valid/Ex_depth', v, k)
+            for k, v in dev_f1_metric.sentence_ex_l.items():
+                self.writer.add_scalar('valid/Ex_length', v, k)
+                
+            # # F1 score for each depth
+            # for k, v in dev_left_metric.sentence_uf1_d.items():
+            #     self.writer.add_scalar('valid/f1_left_depth', v, k)
+            # # F1 score for each length
+            # for k, v in dev_left_metric.sentence_ex_d.items():
+            #     self.writer.add_scalar('valid/Ex_left_depth', v, k)
+            for k, v in dev_left_metric.sentence_uf1_l.items():
+                self.writer.add_scalar('valid/f1_left_length', v, k)
+            for k, v in dev_left_metric.sentence_ex_l.items():
+                self.writer.add_scalar('valid/Ex_left_length', v, k)
+
+            # # F1 score for each depth
+            # for k, v in dev_right_metric.sentence_uf1_d.items():
+            #     self.writer.add_scalar('valid/f1_right_depth', v, k)
+            # # F1 score for each length
+            # for k, v in dev_right_metric.sentence_ex_d.items():
+            #     self.writer.add_scalar('valid/Ex_right_depth', v, k)
+            for k, v in dev_right_metric.sentence_uf1_l.items():
+                self.writer.add_scalar('valid/f1_right_length', v, k)
+            for k, v in dev_right_metric.sentence_ex_l.items():
+                self.writer.add_scalar('valid/Ex_right_length', v, k)
 
             # distribution of estimated span depth
             self.estimated_depth = dict(sorted(self.estimated_depth.items()))
@@ -133,8 +155,12 @@ class Train(CMD):
             if dev_ll > best_metric:
                 best_metric = dev_ll 
                 best_e = epoch
+                checkpoint = {
+                    'epoch': epoch,
+                    'model': self.model.state_dict()
+                }
                 torch.save(
-                   obj=self.model.state_dict(),
+                   obj=checkpoint,
                    f = args.save_dir + "/best.pt"
                 )
                 log.info(f"{t}s elapsed (saved)\n")
@@ -143,7 +169,14 @@ class Train(CMD):
 
             total_time += t
             if train_arg.patience > 0 and epoch - best_e >= train_arg.patience:
-                break
+                if hasattr(self.train_arg, 'change') and self.train_arg.change:
+                    self.train_arg.change = False
+                    self.partition = not self.partition
+                    best_metric = LikelihoodMetric()
+                    best_metric.total_likelihood = -float('inf')
+                    best_metric.total = 1
+                else:
+                    break
         
         self.writer.flush()
         self.writer.close()
