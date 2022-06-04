a29c8065716a9cc5d00c30039867ad8efb98f223
juhyeon <harryjhnam@gm.gist.ac.kr>
2022-06-04 13:54:16 +0000

    add mode for data of evaluation.py

--- evaluate.py
+++ evaluate.py
@@ -10,10 +10,11 @@ import click
 
 @click.command()
 @click.option("--eval_dep", default=False, help="evaluate dependency, only for N(B)L-PCFG")
+@click.option("--data_split", default='test')
 @click.option("--decode_type", default='mbr', help="viterbi or mbr")
 @click.option("--load_from_dir", default="")
 @click.option("--device", '-d', default='0')
-def main(eval_dep, decode_type, load_from_dir, device):
+def main(eval_dep, data_split, decode_type, load_from_dir, device):
     yaml_cfg = yaml.load(open(load_from_dir + "/config.yaml", 'r'))
     args = edict(yaml_cfg)
     args.device = device
@@ -23,7 +24,7 @@ def main(eval_dep, decode_type, load_from_dir, device):
     args.device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
 
     command = Evaluate()
-    command(args, decode_type=decode_type, eval_dep=eval_dep)
+    command(args, data_split=data_split, decode_type=decode_type, eval_dep=eval_dep)
 
 
 if __name__ == '__main__':
