608f5d1b11a0c8c84bd702f7781588389817e491
Jinwook Park <jinwookpark2296@gmail.com>
2022-09-26 15:00:27 +0900

    Feat(reproducible): Add reproducible save & load
    we add save and load function for reproducibility that save (load)
    RNG state of python, numpy, pytorch and CUDA.
    
    And we also update docs of 'fix_seed' function.

--- torch_support/train_support.py
+++ torch_support/train_support.py
@@ -43,18 +43,35 @@ def setup_log_dir(path, parents=True, exist_ok=True):
             print(f'log dir [{p}] is created.')
 
 def fix_seed(
-        seed,
-        python_seed=True,
-        numpy_seed=True,
-        torch_seed=True,
-        cuda_seed=True,
-        worker_init_fn=False,
-        generator=False,
+        seed: int,
+        python_seed: bool=True,
+        numpy_seed: bool=True,
+        torch_seed: bool=True,
+        cuda_seed: bool=True,
+        worker_init_fn: bool=False,
+        generator: bool=False,
+        deterministic: bool=True,
+        benchmark: bool=False,
+        work_space_config: str=":16:8"
     ):
-    '''
-    If you want to fix seed,
-    you have to call this function before assigning variables that you want to fix.
-    '''
+    """If you want to fix seed, you have to call this function before assigning
+    variables that you want to fix.
+
+    Args:
+        seed (int): A random seed that used to fix all seeds.
+        python_seed (bool, optional): Fix python random. Defaults to True.
+        numpy_seed (bool, optional): Fix numpy random. Defaults to True.
+        torch_seed (bool, optional): Fix pytorch random. Defaults to True.
+        cuda_seed (bool, optional): Fix CUDA random. Defaults to True.
+        worker_init_fn (bool, optional): Return worker_init_fn if True. Defaults to False.
+        generator (bool, optional): Return generator if True. Defaults to False.
+        deterministic (bool, optional): Set deterministic. Defaults to True.
+        benchmark (bool, optional): Set benchmark. Defaults to False.
+        work_space_config (_type_, optional): Set work space config. Defaults to ":16:8".
+
+    Returns:
+        _type_: _description_
+    """
     # Python
     if python_seed:
         random.seed(seed)
@@ -65,18 +82,20 @@ def fix_seed(
     # Pytorch
     if torch_seed:
         torch.manual_seed(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = deterministic
+        torch.backends.cudnn.benchmark = benchmark
     # CUDA
     if cuda_seed:
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         if torch.version.cuda >= str(10.2):
-            os.environ['CUBLAS_WORKSPACE_CONFIG']=':16:8'
-            # or
-            # os.environ['CUBLAS_WORKSPACE_CONFIG']=':4096:2'
+            os.environ['CUBLAS_WORKSPACE_CONFIG']=work_space_config
+            # or ":4096:2"
         else:
-            os.environ['CUDA_LAUNCH_BLOCKING']='1'
+            os.environ['CUDA_LAUNCH_BLOCKING']="1"
+
+    worker_init_fn = None
+    generator = None
 
     if worker_init_fn:
         def seed_worker(worker_id):
@@ -107,22 +126,31 @@ def create_save_path(args):
     shutil.copyfile(args.conf, args.save_dir + "/config.yaml")
     os.makedirs(args.save_dir + "/parser")
     copy_tree("parser/", args.save_dir + "/parser")
-    return  saved_name
+    return saved_name
 
 def get_logger(args, log_name='train',path=None):
+    # create logger
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
+
+    # Set formatter
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
+
+    # create file handler
     handler = logging.FileHandler(os.path.join(args.save_dir if path is None else path, '{}.log'.format(log_name)), 'w')
     handler.setLevel(logging.INFO)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+
+    # create console handler
     console = logging.StreamHandler()
     console.setLevel(logging.INFO)
     console.setFormatter(formatter)
     logger.addHandler(console)
+
     logger.propagate = False
     logger.info(args)
     return logger
@@ -172,6 +200,62 @@ def command_decorator(command):
         return result
     return wrapper
 
+def reproducible_save(
+        model, optimizer, save_name,
+        scheduler=None,
+        reproducible=True,
+        **kwargs
+    ):
+    checkpoint = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+    checkpoint.update({k: v for k, v in kwargs.items() if v is not None})
+    
+    if scheduler is not None:
+        checkpoint.update({'scheduler': scheduler.state_dict()})
+    if reproducible:
+        checkpoint.update({
+            'random.python': random.getstate(),
+            'random.numpy': np.random.get_state(),
+            'random.torch': torch.random.get_rng_state(),
+            'random.cuda': torch.cuda.get_rng_state(),
+            'random.cuda_all': torch.cuda.get_rng_state_all(),
+        })
+    torch.save(checkpoint, save_name)
+
+def reproducible_load(
+        model, optimizer, load_name,
+        scheduler=None,
+        reproducible=True,
+    ):
+    # Load checkpoint
+    checkpoint = torch.load(load_name)
+
+    # Load model
+    model.load_state_dict(checkpoint.pop('model'))
+    optimizer.load_state_dict(checkpoint.pop('optimizer'))
+    
+    if scheduler is not None:
+        try:
+            scheduler.load_state_dict(checkpoint.pop('scheduler'))
+        except:
+            print("Warning: scheduler is not loaded.")
+
+    if reproducible:
+        try:
+            random.setstate(checkpoint['random.python'])
+            np.random.set_state(checkpoint['random.numpy'])
+            torch.random.set_rng_state(checkpoint['random.torch'])
+            torch.cuda.set_rng_state(checkpoint['random.cuda'])
+            torch.cuda.set_rng_state_all(checkpoint['random.cuda_all'])
+        except:
+            print("Warning: reproducible is not loaded.")
+
+    if scheduler is not None:
+        return model, optimizer, scheduler, checkpoint
+    else:
+        return model, optimizer, checkpoint
 class MetricDict:
     def __init__(self, metrics) -> None:
         if not isinstance(metrics, dict):
