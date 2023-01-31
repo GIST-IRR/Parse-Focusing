94f33bc50c9f8a100543c2a4d0a21713e2b88bd0
Jinwook Park <jinwookpark2296@gmail.com>
2023-01-31 14:31:41 +0900

    Feature(APCFG): Add AttentionPCFG

--- parser/model/__init__.py
+++ parser/model/__init__.py
@@ -3,8 +3,14 @@ from .NeuralPCFG import NeuralPCFG
 from .TNPCFG import TNPCFG
 from .NeuralBLPCFG import NeuralBLPCFG
 from .NeuralLPCFG import NeuralLPCFG
+from .AttentionCPCFG import AttentionCPCFG
 
 
 __all__ = [
-    CompoundPCFG, NeuralPCFG, TNPCFG, NeuralBLPCFG, NeuralLPCFG
+    CompoundPCFG,
+    NeuralPCFG,
+    TNPCFG,
+    NeuralBLPCFG,
+    NeuralLPCFG,
+    AttentionCPCFG
 ]
