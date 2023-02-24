9763af7996a52331001834b56536ee7c02166db6
Jinwook Park <jinwookpark2296@gmail.com>
2023-02-24 17:30:51 +0900

    feat(data): Update tools for data analysis

--- data_distribution.py
+++ data_distribution.py
@@ -4,6 +4,13 @@ from collections import OrderedDict
 from numpy import vsplit
 from torch.utils.tensorboard import SummaryWriter
 
+from fastNLP.core.dataset import DataSet
+from fastNLP.core.vocabulary import Vocabulary
+import torch
+from utils import (
+    span_to_tree
+)
+
 
 def split_by_key(data, key):
     # split data to ordered dictionary by key
@@ -26,15 +33,121 @@ def count_value(data, key):
             result[n] = 1
     return OrderedDict(sorted(result.items()))
 
+def get_word_freq(dataset):
+    # Word embedding from vocab frequency
+    def clean_word(words):
+        import re
+        def clean_number(w):
+            new_w = re.sub('[0-9]{1,}([,.]?[0-9]*)*', 'N', w)
+            return new_w
+        return [clean_number(word.lower()) for word in words]
+
+    new_words = []
+    for w in dataset['word']:
+        w = clean_word(w)
+        new_words.append(w)
+    dataset['word'] = new_words
+
+    vocab = {}
+    for sent in dataset['word']:
+        for w in sent:
+            if w in vocab:
+                vocab[w] += 1
+            else:
+                vocab[w] = 1
+    vocab = OrderedDict(sorted(vocab.items(), key=lambda x: x[1], reverse=True))
+    vocab = {k: i+2 for i, k in enumerate(vocab.keys()) if i < 10000}
+    vocab.update({'<pad>': 0, '<unk>': 1})
+
+    pos_count_dict = {}
+    for i, sent in enumerate(dataset['word']):
+        pos = dataset['pos'][i]
+        for w, p in zip(sent, pos):
+            if w not in vocab:
+                w = 1
+            else:
+                w = vocab[w]
+
+            if p not in pos_count_dict:
+                pos_count_dict[p] = [0] * len(vocab)
+            
+            pos_count_dict[p][w] += 1
+    pos_count_dict = OrderedDict(
+        sorted(pos_count_dict.items(), key=lambda x: sum(x[1]))
+    )
+    pos_count_dict = torch.tensor(
+        list(pos_count_dict.values()), dtype=torch.float32
+    )
+    pos_count_dict = pos_count_dict.log_softmax(-1)
+
+    torch.save(pos_count_dict, f'weights/terms.pt')
+
+def get_prod_freq(dataset):
+    for i, tree in enumerate(dataset['gold_tree']):
+        tree = span_to_tree(tree)
+        leaf_pos = tree.treepositions('leaves')
+        for j, p in enumerate(leaf_pos):
+            tree[p] = dataset['word'][i][j]
+        prod = tree.productions()
+        print(prod)
+
+
+def get_symbols(production):
+    production = production.split(' ')
+    parent = production[0]
+    left_child = production[2]
+    if len(production) == 4:
+        right_child = production[3]
+    else:
+        right_child = None
+    return parent, left_child, right_child
+
+def get_parse_tree_freq(trees):
+    total_length = len(trees)
+    parse_trees = []
+    lengths = {}
+    for tree in trees:
+        tree = [s[:2] for s in tree]
+        tree = span_to_tree(tree)
+        # dist tree length
+        length = len(tree.leaves())
+        if length in lengths:
+            lengths[length] += 1
+        else:
+            lengths[length] = 1
+
+        # dist tree type
+        # symbol_pos = set(tree.treepositions()) - \
+        #     set(tree.treepositions('leaves'))
+        # for i in symbol_pos:
+        #     label = tree[i].label()
+        #     if label == "'T'":
+        #         tree[i].set_label('T')
+        #     else:
+        #         tree[i].set_label('NT')
+
+        # Add to parse tree list
+        if tree not in parse_trees:
+            parse_trees.append(tree)
+
+    sorted_type = {}
+    for tree in parse_trees:
+        tree_length = len(tree.leaves())
+        if tree_length not in sorted_type:
+            sorted_type[tree_length] = [tree]
+        else:
+            sorted_type[tree_length].append(tree)
+    print(lengths)
+
 
 def main(args):
     data_dir = args.dir
-    lang = 'french'
+    lang = 'edit-english'
     split = 'standard'
-    form = 'nocnf'
+    form = 'both'
     data_type = f'{lang}.{split}.{form}'
     # data_type = f'{split}.{form}'
-    data_split = 'test'
+    data_split = 'train'
     data_name = f'{data_dir}/{data_type}/{lang}-{split}-{data_split}.pkl'
     # data_name = f'{data_dir}/{data_type}/chinese-{data_split}.pkl'
     with open(data_name, 'rb') as f:
@@ -42,16 +155,20 @@ def main(args):
 
     sort_type = 'depth'
 
-    # dataset transform to dict for each sentence.
-    data = []
-    for i in range(len(dataset['word'])):
-        data.append({})
+    # get_word_freq(dataset)
+    # get_parse_tree_freq(dataset['gold_tree'])
+    get_prod_freq(dataset)
 
-    for k, vs in dataset.items():
-        for i, v in enumerate(vs):
-            if k == 'word':
-                data[i].update({'len': len(v)})
-            data[i].update({k: v})
+    # dataset transform to dict for each sentence.
+    # data = []
+    # for i in range(len(dataset['word'])):
+    #     data.append({})
+
+    # for k, vs in dataset.items():
+    #     for i, v in enumerate(vs):
+    #         if k == 'word':
+    #             data[i].update({'len': len(v)})
+    #         data[i].update({k: v})
 
     # criterion = 40
     # data = list(filter(lambda x: len(x['word']) <= criterion, data))
@@ -65,21 +182,21 @@ def main(args):
     # length_dist = count_value(data, 'depth')
 
     # depth distribution for each length
-    split_dict = split_by_key(data, 'len')
-    n_dict = {}
-    for k in split_dict.keys():
-        n_dict[k] = count_value(split_dict[k], 'depth')
+    # split_dict = split_by_key(data, 'len')
+    # n_dict = {}
+    # for k in split_dict.keys():
+    #     n_dict[k] = count_value(split_dict[k], 'depth')
 
-    log_name = 'log/filter_depth_dist'
-    writer = SummaryWriter(log_name)
+    # log_name = 'log/filter_depth_dist'
+    # writer = SummaryWriter(log_name)
 
-    for k, vs in n_dict.items():
-        tag = f'{data_type}/{data_split}/length_{k}'
-        for k, v in vs.items():
-            writer.add_scalar(tag, v, k)
+    # for k, vs in n_dict.items():
+    #     tag = f'{data_type}/{data_split}/length_{k}'
+    #     for k, v in vs.items():
+    #         writer.add_scalar(tag, v, k)
 
-    writer.flush()
-    writer.close()
+    # writer.flush()
+    # writer.close()
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
--- preprocessing.py
+++ preprocessing.py
@@ -1,39 +1,66 @@
-from ntpath import join
 from nltk.tree import Tree
+from nltk.grammar import Nonterminal
+
+from fastNLP.core.dataset import DataSet
+from fastNLP.core.vocabulary import Vocabulary
+
 import argparse
 import pickle
+from collections import defaultdict
 
 import os
-import logging
-import copy
+from typing import List, Union
+
+import torch
 
 
-def get_trees(file):
+def get_trees(file, cnf=False):
+    r"""Get parse trees from sentences in file.
+    
+    Args:
+        file (str): path to file containing parse trees.
+        cnf (bool): whether to convert trees to Chomsky Normal Form (CNF).
+
+    Returns:
+        list of nltk.tree.Tree: list of parse trees.
+
+    Examples:
+        >>> trees = get_trees('data/ptb/train.txt')
+    """
     trees = []
     with open(file, 'r') as f:
         for line in f:
             tree = Tree.fromstring(line)
+            if cnf:
+                tree.chomsky_normal_form(horzMarkov=0, vertMarkov=1)
             trees.append(tree)
     return trees
 
-def get_cnf_trees(file):
-    trees = []
-    with open(file, 'r') as f:
-        for line in f:
-            tree = Tree.fromstring(line)
-            tree.chomsky_normal_form(horzMarkov=0, vertMarkov=1)
-            trees.append(tree)
-    return trees
+def trees_to_cnf(
+    trees: Union[Tree, List[Tree]],
+    copy=True, factor='right', horzMarkov=None, vertMarkov=0
+):
+    r"""transform trees to Chomsky Normal Form (CNF).
 
-def tree_to_cnf(tree, factor='right', horzMarkov=None, vertMarkov=0):
-    tree = tree.copy(deep=True)
-    tree.chomsky_normal_form(factor=factor, horzMarkov=horzMarkov, vertMarkov=vertMarkov)
-    return tree
+    Args:
+        trees (nltk.tree.Tree or list of nltk.tree.Tree): trees to be transformed.
+        copy (bool): whether to copy trees before transforming.
+        factor (str): factorization direction, 'right' or 'left'.
+        horzMarkov (int): horizontal Markovization order.
+        vertMarkov (int): vertical Markovization order.
 
-def trees_to_cnf(trees, factor='right', horzMarkov=None, vertMarkov=0):
+    Returns:
+        list of nltk.tree.Tree: list of transformed trees.
+    """
+    if isinstance(trees, Tree):
+        trees = [trees]
     result = []
     for t in trees:
-        t = tree_to_cnf(t, factor=factor, horzMarkov=horzMarkov, vertMarkov=vertMarkov)
+        if copy:
+            t = t.copy(deep=True)
+        t.chomsky_normal_form(
+            factor=factor, horzMarkov=horzMarkov, vertMarkov=vertMarkov
+        )
         result.append(t)
     return result
 
@@ -50,7 +77,6 @@ def tree_transform(
         tree.collapse_unary(collapsePOS=collapsePOS)
     tree.chomsky_normal_form(factor=factor)
     return tree
-
 
 def factorize(tree):
     def track(tree, i):
@@ -105,20 +131,20 @@ def create_dataset_from_trees(trees, depth=False, cnf='none', collapse=False):
         pos_array.append(pos)
         gold_trees.append(factorize(tree))
         if cnf == 'both':
-            left_tree = tree_to_cnf(col_tree, factor='left')
+            left_tree = trees_to_cnf(col_tree, factor='left')
             gold_trees_left.append(factorize(left_tree))
-            right_tree = tree_to_cnf(col_tree, factor='right')
+            right_tree = trees_to_cnf(col_tree, factor='right')
             gold_trees_right.append(factorize(right_tree))
             if depth:
                 depth_left.append(left_tree.height())
                 depth_right.append(right_tree.height())
         elif cnf == 'left':
-            left_tree = tree_to_cnf(col_tree, factor='left')
+            left_tree = trees_to_cnf(col_tree, factor='left')
             gold_trees_left.append(factorize(left_tree))
             if depth:
                 depth_left.append(left_tree.height())
         elif cnf == 'right':
-            right_tree = tree_to_cnf(col_tree, factor='right')
+            right_tree = trees_to_cnf(col_tree, factor='right')
             gold_trees_right.append(factorize(right_tree))
             if depth:
                 depth_right.append(right_tree.height())
@@ -146,7 +172,173 @@ def create_dataset_from_trees(trees, depth=False, cnf='none', collapse=False):
 
     return result
 
+def create_probability_distribution(
+    dir, prefix, postfix, collapsePos=True, factor='right'
+    ):
+    file = os.path.join(dir, f'{prefix}-{postfix}.txt')
+
+    print('[INFO] Load dataset...', end='')
+    trees = get_trees(file)
+    print('Done')
+
+    # Define criterion for splitting
+    def depth(tree):
+        return tree.height()
+    def length(tree):
+        return len(tree.leaves())
+
+    print('[INFO] Status Test')
+    print(
+        f'{postfix} set contain : total {len(trees)}\n'
+        f'\tLength: {min(map(length, trees))} - {max(map(length, trees))}\n'
+        f'\tDepth: {min(map(depth, trees))} - {max(map(depth, trees))}'
+    )
+
+    root_productions = defaultdict(int)
+    rule_productions = defaultdict(int)
+    term_productions = defaultdict(int)
+
+    def clean_word(words):
+        import re
+        def clean_number(w):
+            new_w = re.sub('[0-9]{1,}([,.]?[0-9]*)*', 'N', w)
+            return new_w
+        return [clean_number(word.lower()) for word in words]
+
+    def remove_indicator(p, indicator='*'):
+        p._lhs._symbol = p.lhs().symbol().split(indicator)[-1]
+        p._lhs._symbol = p.lhs().symbol().split('|')[0]
+
+        if isinstance(p.rhs()[0], Nonterminal):
+            p._rhs[0]._symbol = p.rhs()[0].symbol().split(indicator)[-1]
+            p._rhs[0]._symbol = p.rhs()[0].symbol().split('|')[0]
+            pass
+        else:
+            p._rhs = (clean_word([p.rhs()[0]])[0], )
+
+        if len(p.rhs()) == 2:
+            p._rhs[1]._symbol = p.rhs()[1].symbol().split(indicator)[-1]
+            p._rhs[1]._symbol = p.rhs()[1].symbol().split('|')[0]
+            pass
+
+        return p
+    
+    words = [t.leaves() for t in trees]
+    dataset = DataSet()
+    dataset.add_field('word', words)
+    dataset.apply_field(clean_word, 'word', 'word')
+    vocab = Vocabulary(max_size=10000)
+    vocab.from_dataset(dataset, field_name='word')
+
+    for tree in trees:
+        tree.chomsky_normal_form(factor=factor, horzMarkov=0, vertMarkov=0)
+        tree.collapse_unary(collapsePOS=collapsePos, joinChar='*')
+        prods = tree.productions()
+        for p in prods:
+            p = remove_indicator(p)
+            # lhs = p.lhs()
+
+            # Check whether the production is a unary or binary
+            if len(p.rhs()) == 1:
+                rhs = p.rhs()[0]
+                if isinstance(rhs, str):
+                    # Unary Rules
+                    term_productions[p] += 1
+                else:
+                    # ROOT -> S
+                    root_productions[p] += 1
+            elif len(p.rhs()) == 2:
+                rule_productions[p] += 1
+    
+    # Count symbols
+    nonterminals = defaultdict(int)
+    terminals = defaultdict(int)
+    # vocabulary = defaultdict(int)
+
+    for p in term_productions:
+        lhs = p.lhs()
+        rhs = p.rhs()[0]
+        terminals[lhs] += 1
+        # vocabulary[rhs] += 1
+
+    for p in rule_productions:
+        lhs = p.lhs()
+        rhs = p.rhs()
+        if lhs in terminals:
+            lhs._symbol = 'NT-' + lhs.symbol()
+        nonterminals[lhs] += 1
+        if rhs[0] not in terminals:
+            nonterminals[rhs[0]] += 1
+        else:
+            terminals[rhs[0]] += 1
+        if rhs[1] not in terminals:
+            nonterminals[rhs[1]] += 1
+        else:
+            terminals[rhs[1]] += 1
+
+    for p in root_productions:
+        rhs = p.rhs()[0]
+        if rhs not in terminals:
+            nonterminals[rhs] += 1
+    
+    # Construct the probability distribution
+    nonterminals = list(dict(sorted(
+        nonterminals.items(), key=lambda x: x[1], reverse=True
+    )).keys())
+    terminals = list(dict(sorted(
+        terminals.items(), key=lambda x: x[1], reverse=True
+    )).keys())
+    # vocabulary = list(dict(sorted(
+    #     vocabulary.items(), key=lambda x: x[1], reverse=True
+    # )).keys())
+
+    symbols = {s: i for i, s in enumerate(nonterminals)}
+    num_nt = len(nonterminals)
+    num_t = len(terminals)
+    terminals = {s: i+num_nt for i, s in enumerate(terminals)}
+    symbols.update(terminals)
+    # tokens = {'<pad>': 0, '<unk>': 1}
+    # tokens.update({s: i+2 for i, s in enumerate(vocabulary) if i < 10000})
+    print(f'[INFO] Nonterminal size: {num_nt}')
+    print(f'[INFO] Terminal size: {num_t}')
+
+    # Symbol productions to index productions
+    root_pd = [[0] * num_nt]
+    for p, c in root_productions.items():
+        rhs_idx = symbols[p.rhs()[0]]
+        if rhs_idx >= num_nt:
+            continue
+        root_pd[0][rhs_idx] = c
+
+    rule_pd = [
+        [[0] * len(symbols) for _ in range(len(symbols))]
+        for _ in range(len(nonterminals))
+    ]
+    for p, c in rule_productions.items():
+        lhs_idx = symbols[p.lhs()]
+        rhs_l_idx = symbols[p.rhs()[0]]
+        rhs_r_idx = symbols[p.rhs()[1]]
+        rule_pd[lhs_idx][rhs_l_idx][rhs_r_idx] = c
+
+    term_pd = [[0] * len(vocab) for _ in range(num_t)]
+    for p, c in term_productions.items():
+        lhs_idx = symbols[p.lhs()] - num_nt
+        rhs_idx = vocab.word2idx.get(p.rhs()[0], 1)
+        term_pd[lhs_idx][rhs_idx] += c
+
+    root_pd = torch.tensor(root_pd).float()
+    rule_pd = torch.tensor(rule_pd).float()
+    term_pd = torch.tensor(term_pd).float()
+
+    torch.save(root_pd, f'weights/{prefix}_xbar_{factor}_root_pd.pt')
+    torch.save(rule_pd, f'weights/{prefix}_xbar_{factor}_rule_pd.pt')
+    torch.save(term_pd, f'weights/{prefix}_xbar_{factor}_term_pd.pt')
+    print("Done.")
+
 def redistribution(args):
+    create_probability_distribution(
+        args.dir, args.prefix, 'train', factor=args.factor)
+    exit()
     # Get path for the dataset
     train_file = os.path.join(args.dir, f'{args.prefix}-train.txt')
     valid_file = os.path.join(args.dir, f'{args.prefix}-valid.txt')
@@ -182,21 +374,12 @@ def redistribution(args):
 
     # Status Test
     print('[INFO] Status Test')
-    print(
-        f'train set contain : total {len(train_trees)}\n'
-        f'\tLength: {min(map(length, train_trees))} - {max(map(length, train_trees))}\n'
-        f'\tDepth: {min(map(depth, train_trees))} - {max(map(depth, train_trees))}'
-    )
-    print(
-        f'valid set contain : total {len(valid_trees)}\n'
-        f'\tLength: {min(map(length, valid_trees))} - {max(map(length, valid_trees))}\n'
-        f'\tDepth: {min(map(depth, valid_trees))} - {max(map(depth, valid_trees))}'
-    )
-    print(
-        f'test set contain : total {len(test_trees)}\n'
-        f'\tLength: {min(map(length, test_trees))} - {max(map(length, test_trees))}\n'
-        f'\tDepth: {min(map(depth, test_trees))} - {max(map(depth, test_trees))}'
-    )
+    for trees in [train_trees, valid_trees, test_trees]:
+        print(
+            f'train set contain : total {len(trees)}\n'
+            f'\tLength: {min(map(length, trees))} - {max(map(length, trees))}\n'
+            f'\tDepth: {min(map(depth, trees))} - {max(map(depth, trees))}'
+        )
 
     # Check splitting criterion
     if args.criterion != 'standard':
@@ -227,18 +410,11 @@ def redistribution(args):
         test_trees = trees[split[1]:]
 
         # Print dataset status
-        print(
-            f'train set contain : total {len(train_trees)}\n'
-            f'\t{args.criterion}: {min(map(criterion, train_trees))} - {max(map(criterion, train_trees))}'
-        )
-        print(
-            f'valid set contain : total {len(valid_trees)}\n'
-            f'\t{args.criterion}: {min(map(criterion, valid_trees))} - {max(map(criterion, valid_trees))}'
-        )
-        print(
-            f'test set contain : total {len(test_trees)}\n'
-            f'\t{args.criterion}: {min(map(criterion, test_trees))} - {max(map(criterion, test_trees))}'
-        )
+        for trees in [train_trees, valid_trees, test_trees]:
+            print(
+                f'train set contain : total {len(trees)}\n'
+                f'\t{args.criterion}: {min(map(criterion, trees))} - {max(map(criterion, trees))}'
+            )
 
     # Check save path and create if not exist
     print(f'[INFO] Dataset will be saved on {args.cache_path}.')
@@ -249,15 +425,18 @@ def redistribution(args):
 
     # Saving datasets
     print('[INFO] Saving dataset...', end='')
-    result = create_dataset_from_trees(train_trees, depth=args.target_depth, cnf=args.cnf, collapse=args.collapse)
+    tree_size = len(train_trees) // 8
+    result = create_dataset_from_trees(train_trees[:tree_size], depth=args.target_depth, cnf=args.cnf, collapse=args.collapse)
     with open(os.path.join(args.cache_path, f"{args.prefix}-{args.criterion}-train.pkl"), "wb") as f:
         pickle.dump(result, f)
 
-    result = create_dataset_from_trees(valid_trees, depth=args.target_depth, cnf=args.cnf, collapse=args.collapse)
+    tree_size = len(valid_trees) // 8
+    result = create_dataset_from_trees(valid_trees[:tree_size], depth=args.target_depth, cnf=args.cnf, collapse=args.collapse)
     with open(os.path.join(args.cache_path, f"{args.prefix}-{args.criterion}-valid.pkl"), "wb") as f:
         pickle.dump(result, f)
 
-    result = create_dataset_from_trees(test_trees, depth=args.target_depth, cnf=args.cnf, collapse=args.collapse)
+    tree_size = len(test_trees) // 8
+    result = create_dataset_from_trees(test_trees[:tree_size], depth=args.target_depth, cnf=args.cnf, collapse=args.collapse)
     with open(os.path.join(args.cache_path, f"{args.prefix}-{args.criterion}-test.pkl"), "wb") as f:
         pickle.dump(result, f)
     print('DONE.')
@@ -270,6 +449,9 @@ if __name__ == '__main__':
         description='preprocess ptb file.'
     )
     parser.add_argument('--dir', required=True)
+    parser.add_argument('--train', default=False)
+    parser.add_argument('--valid', default=False)
+    parser.add_argument('--test', default=True)
     parser.add_argument('--prefix', default='english')
     parser.add_argument('--cache_path', default='data/')
     parser.add_argument('--criterion', default='standard', choices=['standard', 'depth', 'length', 'binarized-depth', 'collapsed-depth', 'binarized-collapsed-depth'])
