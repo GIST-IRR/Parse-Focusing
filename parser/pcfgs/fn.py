e1ae1447f5b07307b134cbcc0dccfdb10a140a38
Jinwook Park <jinwookpark2296@gmail.com>
2024-08-07 20:30:46 +0900

    fix: checkpoint

--- parser/pcfgs/fn.py
+++ parser/pcfgs/fn.py
@@ -1,21 +1,25 @@
 from torch.utils.checkpoint import checkpoint as ckp
 
-'''
+"""
 To save memory of the inside algorithm.
-'''
+"""
+
+
 def checkpoint(func):
     def wrapper(*args, **kwargs):
-        return ckp(func, *args, **kwargs, use_reentract=True)
+        return ckp(func, *args, **kwargs, use_reentrant=True)
+
     return wrapper
 
 
-
-'''
+"""
 I borrow the idea from:
 https://github.com/yzhangcs/parser/blob/a8e6f443febf8d986cd6eba74966fdf924cb567d/supar/utils/fn.py#L32
 to implement `stripe' function. If you do not understand the code, plz refer to their code and comment.
 Roughly speaking, this function packs all inside scores of spans of same width w, which facilitates parallel computation.      
-'''
+"""
+
+
 def stripe(x, n, w, offset=(0, 0), dim=1):
     x, seq_len = x.contiguous(), x.size(2)
     stride = list(x.stride())
@@ -23,14 +27,19 @@ def stripe(x, n, w, offset=(0, 0), dim=1):
     stride[1] = (seq_len + 1) * numel
     stride[2] = (1 if dim == 1 else seq_len) * numel
     if len(x.shape) > 3:
-        return x.as_strided(size=(x.shape[0], n, w, *list(x.shape[3:])),
-                            stride=stride,
-                            storage_offset=(offset[0] * seq_len + offset[1]) * numel)
+        return x.as_strided(
+            size=(x.shape[0], n, w, *list(x.shape[3:])),
+            stride=stride,
+            storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+        )
     else:
-        return x.as_strided(size=(x.shape[0], n, w),
-                            stride=stride,
-                            storage_offset=(offset[0] * seq_len + offset[1]) * numel)
-    
+        return x.as_strided(
+            size=(x.shape[0], n, w),
+            stride=stride,
+            storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+        )
+
+
 def stripe_add_(x, y, n, w, offset=(0, 0), dim=1):
     x, seq_len = x.contiguous(), x.size(2)
     stride = list(x.stride())
@@ -38,32 +47,46 @@ def stripe_add_(x, y, n, w, offset=(0, 0), dim=1):
     stride[1] = (seq_len + 1) * numel
     stride[2] = (1 if dim == 1 else seq_len) * numel
     if len(x.shape) > 3:
-        tmp = x.as_strided(size=(x.shape[0], n, w, *list(x.shape[3:])),
-                           stride=stride,
-                           storage_offset=(offset[0] * seq_len + offset[1]) * numel)
-        x.as_strided(size=(x.shape[0], n, w, *list(x.shape[3:])),
-                     stride=stride,
-                     storage_offset=(offset[0] * seq_len + offset[1]) * numel).copy_(tmp + y)
+        tmp = x.as_strided(
+            size=(x.shape[0], n, w, *list(x.shape[3:])),
+            stride=stride,
+            storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+        )
+        x.as_strided(
+            size=(x.shape[0], n, w, *list(x.shape[3:])),
+            stride=stride,
+            storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+        ).copy_(tmp + y)
     else:
-        tmp = x.as_strided(size=(x.shape[0], n, w),
-                           stride=stride,
-                           storage_offset=(offset[0] * seq_len + offset[1]) * numel)
-        x.as_strided(size=(x.shape[0], n, w),
-                     stride=stride,
-                     storage_offset=(offset[0] * seq_len + offset[1]) * numel).copy_(tmp + y)
+        tmp = x.as_strided(
+            size=(x.shape[0], n, w),
+            stride=stride,
+            storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+        )
+        x.as_strided(
+            size=(x.shape[0], n, w),
+            stride=stride,
+            storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+        ).copy_(tmp + y)
 
-'''
+
+"""
 This function is similar to the above one, but additionally select head words, used in lexicalized PCFGs.
-'''
+"""
+
+
 def stripe_with_headword(x, n, w, offset=(0, 0), dim=1):
     x, seq_len = x.contiguous(), x.size(2)
     stride = list(x.stride())
     numel = stride[2]
     stride[1] = (seq_len + 1) * numel + stride[3]
     stride[2] = (1 if dim == 1 else seq_len) * numel
-    return x.as_strided(size=(x.shape[0], n, w, w+1, *list(x.shape[4:])),
-                            stride=stride,
-                            storage_offset=(offset[0] * seq_len + offset[1]) * numel)
+    return x.as_strided(
+        size=(x.shape[0], n, w, w + 1, *list(x.shape[4:])),
+        stride=stride,
+        storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+    )
+
 
 def stripe_with_headword_add_(x, y, n, w, offset=(0, 0), dim=1):
     x, seq_len = x.contiguous(), x.size(2)
@@ -71,16 +94,23 @@ def stripe_with_headword_add_(x, y, n, w, offset=(0, 0), dim=1):
     numel = stride[2]
     stride[1] = (seq_len + 1) * numel + stride[3]
     stride[2] = (1 if dim == 1 else seq_len) * numel
-    tmp = x.as_strided(size=(x.shape[0], n, w, w + 1, *list(x.shape[4:])),
-                       stride=stride,
-                       storage_offset=(offset[0] * seq_len + offset[1]) * numel)
-    x.as_strided(size=(x.shape[0], n, w, w + 1, *list(x.shape[4:])),
-                 stride=stride,
-                 storage_offset=(offset[0] * seq_len + offset[1]) * numel).copy_(tmp + y)
+    tmp = x.as_strided(
+        size=(x.shape[0], n, w, w + 1, *list(x.shape[4:])),
+        stride=stride,
+        storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+    )
+    x.as_strided(
+        size=(x.shape[0], n, w, w + 1, *list(x.shape[4:])),
+        stride=stride,
+        storage_offset=(offset[0] * seq_len + offset[1]) * numel,
+    ).copy_(tmp + y)
 
-'''
+
+"""
 used in bilexicalized-PCFGs.  
-'''
+"""
+
+
 def stripe_grammar_rules(x, n, w, offset=0):
     x, seq_len = x.contiguous(), x.size(2)
     stride = list(x.stride())
@@ -89,8 +119,12 @@ def stripe_grammar_rules(x, n, w, offset=0):
     new_stride.append(stride[1])
     new_stride.append(stride[1])
     new_stride.extend(stride[2:])
-    return x.as_strided(size=(x.shape[0], n, w, *x.shape[2:]),
-                            stride=new_stride, storage_offset=offset*stride[1])
+    return x.as_strided(
+        size=(x.shape[0], n, w, *x.shape[2:]),
+        stride=new_stride,
+        storage_offset=offset * stride[1],
+    )
+
 
 def stripe_grammar_rules_add_(x, y, n, w, offset=0):
     x, seq_len = x.contiguous(), x.size(2)
@@ -100,10 +134,16 @@ def stripe_grammar_rules_add_(x, y, n, w, offset=0):
     new_stride.append(stride[1])
     new_stride.append(stride[1])
     new_stride.extend(stride[2:])
-    tmp = x.as_strided(size=(x.shape[0], n, w, *x.shape[2:]),
-                       stride=new_stride, storage_offset=offset * stride[1])
-    x.as_strided(size=(x.shape[0], n, w, *x.shape[2:]),
-                 stride=new_stride, storage_offset=offset * stride[1]).copy_(tmp + y)
+    tmp = x.as_strided(
+        size=(x.shape[0], n, w, *x.shape[2:]),
+        stride=new_stride,
+        storage_offset=offset * stride[1],
+    )
+    x.as_strided(
+        size=(x.shape[0], n, w, *x.shape[2:]),
+        stride=new_stride,
+        storage_offset=offset * stride[1],
+    ).copy_(tmp + y)
 
 
 def diagonal_copy_depth_old(x, y, w, d):
@@ -114,15 +154,17 @@ def diagonal_copy_depth_old(x, y, w, d):
     new_stride.append(stride[1] + stride[2])
     if len(x.shape) > 3:
         new_stride.extend(stride[3:])
-        x.as_strided(size=(x.shape[0], seq_len - w,  *list(x.shape[3:-1]), d),
-                     stride=new_stride,
-                     storage_offset=(w * stride[2])
-                     ).copy_(y)
+        x.as_strided(
+            size=(x.shape[0], seq_len - w, *list(x.shape[3:-1]), d),
+            stride=new_stride,
+            storage_offset=(w * stride[2]),
+        ).copy_(y)
     else:
-        x.as_strided(size=(x.shape[0], seq_len - w),
-                     stride=new_stride,
-                     storage_offset=(w * stride[2])
-                     ).copy_(y)
+        x.as_strided(
+            size=(x.shape[0], seq_len - w),
+            stride=new_stride,
+            storage_offset=(w * stride[2]),
+        ).copy_(y)
 
 
 def diagonal_copy_depth(x, y, w, d):
@@ -135,15 +177,18 @@ def diagonal_copy_depth(x, y, w, d):
     d_size = max_d - min_d + 1
     if len(x.shape) > 3:
         new_stride.extend(stride[3:])
-        x.as_strided(size=(x.shape[0], seq_len - w,  *list(x.shape[3:-1]), d_size),
-                     stride=new_stride,
-                     storage_offset=(w * stride[2] + min_d * stride[-1])
-                     ).copy_(y)
+        x.as_strided(
+            size=(x.shape[0], seq_len - w, *list(x.shape[3:-1]), d_size),
+            stride=new_stride,
+            storage_offset=(w * stride[2] + min_d * stride[-1]),
+        ).copy_(y)
     else:
-        x.as_strided(size=(x.shape[0], seq_len - w),
-                     stride=new_stride,
-                     storage_offset=(w * stride[2] + min_d * stride[-1])
-                     ).copy_(y)
+        x.as_strided(
+            size=(x.shape[0], seq_len - w),
+            stride=new_stride,
+            storage_offset=(w * stride[2] + min_d * stride[-1]),
+        ).copy_(y)
+
 
 def diagonal_copy_(x, y, w):
     x, seq_len = x.contiguous(), x.size(1)
@@ -153,15 +198,18 @@ def diagonal_copy_(x, y, w):
     new_stride.append(stride[1] + stride[2])
     if len(x.shape) > 3:
         new_stride.extend(stride[3:])
-        x.as_strided(size=(x.shape[0], seq_len - w,  *list(x.shape[3:])),
-                     stride=new_stride,
-                     storage_offset= w * stride[2]
-                     ).copy_(y)
+        x.as_strided(
+            size=(x.shape[0], seq_len - w, *list(x.shape[3:])),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        ).copy_(y)
     else:
-        x.as_strided(size=(x.shape[0], seq_len - w),
-                     stride=new_stride,
-                     storage_offset=w * stride[2]
-                     ).copy_(y)
+        x.as_strided(
+            size=(x.shape[0], seq_len - w),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        ).copy_(y)
+
 
 def diagonal_depth(x, w, d):
     x, seq_len = x.contiguous(), x.size(1)
@@ -173,15 +221,18 @@ def diagonal_depth(x, w, d):
     d_size = max_d - min_d + 1
     if len(x.shape) > 3:
         new_stride.extend(stride[3:])
-        return x.as_strided(size=(x.shape[0], seq_len - w, d_size),
-                            stride=new_stride,
-                            storage_offset=(w * stride[2] + min_d * stride[-1])
-                            )
+        return x.as_strided(
+            size=(x.shape[0], seq_len - w, d_size),
+            stride=new_stride,
+            storage_offset=(w * stride[2] + min_d * stride[-1]),
+        )
     else:
-        return x.as_strided(size=(x.shape[0], seq_len - w, d_size),
-                            stride=new_stride,
-                            storage_offset=(w * stride[2] + min_d * stride[-1])
-                            )
+        return x.as_strided(
+            size=(x.shape[0], seq_len - w, d_size),
+            stride=new_stride,
+            storage_offset=(w * stride[2] + min_d * stride[-1]),
+        )
+
 
 def diagonal(x, w):
     x, seq_len = x.contiguous(), x.size(1)
@@ -191,15 +242,18 @@ def diagonal(x, w):
     new_stride.append(stride[1] + stride[2])
     if len(x.shape) > 3:
         new_stride.extend(stride[3:])
-        return x.as_strided(size=(x.shape[0], seq_len - w, *list(x.shape[3:])),
-                            stride=new_stride,
-                            storage_offset=w * stride[2]
-                            )
+        return x.as_strided(
+            size=(x.shape[0], seq_len - w, *list(x.shape[3:])),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        )
     else:
-        return x.as_strided(size=(x.shape[0], seq_len - w),
-                            stride=new_stride,
-                            storage_offset=w * stride[2]
-                            )
+        return x.as_strided(
+            size=(x.shape[0], seq_len - w),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        )
+
 
 def diagonal_copy_with_headword(x, y, w):
     x, seq_len = x.contiguous(), x.size(1)
@@ -210,16 +264,19 @@ def diagonal_copy_with_headword(x, y, w):
     new_stride.append(stride[3])
     if len(x.shape) > 4:
         new_stride.extend(stride[4:])
-        x.as_strided(size=(x.shape[0], seq_len - w, w, *list(x.shape[4:])),
-                     stride=new_stride,
-                     storage_offset= w * stride[2]
-                     ).copy_(y)
+        x.as_strided(
+            size=(x.shape[0], seq_len - w, w, *list(x.shape[4:])),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        ).copy_(y)
     else:
         new_stride.append(stride[3])
-        x.as_strided(size=(x.shape[0], seq_len - w, w, *list(x.shape[3:])),
-                     stride=new_stride,
-                     storage_offset= w * stride[2]
-                     ).copy_(y)
+        x.as_strided(
+            size=(x.shape[0], seq_len - w, w, *list(x.shape[3:])),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        ).copy_(y)
+
 
 def diagonal_with_headword(x, w):
     x, seq_len = x.contiguous(), x.size(1)
@@ -230,16 +287,19 @@ def diagonal_with_headword(x, w):
     new_stride.append(stride[3])
     if len(x.shape) > 4:
         new_stride.extend(stride[4:])
-        return x.as_strided(size=(x.shape[0], seq_len - w, w, *list(x.shape[4:])),
-                     stride=new_stride,
-                     storage_offset=w * stride[2]
-                     )
+        return x.as_strided(
+            size=(x.shape[0], seq_len - w, w, *list(x.shape[4:])),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        )
     else:
         # new_stride.append(stride[3])
-        return x.as_strided(size=(x.shape[0], seq_len - w, w),
-                     stride=new_stride,
-                     storage_offset=w * stride[2]
-                     )
+        return x.as_strided(
+            size=(x.shape[0], seq_len - w, w),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        )
+
 
 def diagonal_with_headword_add_(x, y, w):
     x, seq_len = x.contiguous(), x.size(1)
@@ -250,24 +310,29 @@ def diagonal_with_headword_add_(x, y, w):
     new_stride.append(stride[3])
     if len(x.shape) > 4:
         new_stride.extend(stride[4:])
-        tmp = x.as_strided(size=(x.shape[0], seq_len - w, w, *list(x.shape[4:])),
-                           stride=new_stride,
-                           storage_offset=w * stride[2]
-                           )
-        x.as_strided(size=(x.shape[0], seq_len - w, w, *list(x.shape[4:])),
-                     stride=new_stride,
-                     storage_offset=w * stride[2]
-                     ).copy_(tmp + y)
+        tmp = x.as_strided(
+            size=(x.shape[0], seq_len - w, w, *list(x.shape[4:])),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        )
+        x.as_strided(
+            size=(x.shape[0], seq_len - w, w, *list(x.shape[4:])),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        ).copy_(tmp + y)
     else:
         # new_stride.append(stride[3])
-        tmp = x.as_strided(size=(x.shape[0], seq_len - w, w),
-                           stride=new_stride,
-                           storage_offset=w * stride[2]
-                           )
-        x.as_strided(size=(x.shape[0], seq_len - w, w),
-                     stride=new_stride,
-                     storage_offset=w * stride[2]
-                     ).copy_(tmp + y)
+        tmp = x.as_strided(
+            size=(x.shape[0], seq_len - w, w),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        )
+        x.as_strided(
+            size=(x.shape[0], seq_len - w, w),
+            stride=new_stride,
+            storage_offset=w * stride[2],
+        ).copy_(tmp + y)
+
 
 # the following three functions are used in implementing Eisner-Satta algorithm...
 def stripe_headed_left(x, n, w, nt, t):
@@ -278,9 +343,11 @@ def stripe_headed_left(x, n, w, nt, t):
     new_stride.append(stride[1] + stride[2])
     new_stride.append(stride[1])
     new_stride.extend(stride[3:])
-    return x.as_strided(size=(x.shape[0], n, w, x.shape[3], nt,  t),
-                            stride=new_stride,
-                            storage_offset= w * stride[2] + nt * stride[-1])
+    return x.as_strided(
+        size=(x.shape[0], n, w, x.shape[3], nt, t),
+        stride=new_stride,
+        storage_offset=w * stride[2] + nt * stride[-1],
+    )
 
 
 def stripe_headed_right(x, n, w, nt, t):
@@ -291,9 +358,12 @@ def stripe_headed_right(x, n, w, nt, t):
     new_stride.append(stride[1] + stride[2])
     new_stride.append(stride[1])
     new_stride.extend(stride[3:])
-    return x.as_strided(size=(x.shape[0], n, w, x.shape[3], t, nt),
-                            stride=new_stride,
-                            storage_offset= stride[1] + nt*stride[-2])
+    return x.as_strided(
+        size=(x.shape[0], n, w, x.shape[3], t, nt),
+        stride=new_stride,
+        storage_offset=stride[1] + nt * stride[-2],
+    )
+
 
 def stripe_need_dad(x, n, w, start, end, headstart):
     x = x.contiguous()
@@ -303,8 +373,14 @@ def stripe_need_dad(x, n, w, start, end, headstart):
     new_stride.append(stride[1] + stride[2] + stride[3])
     new_stride.append(stride[3])
     new_stride.extend(stride[4:])
-    return x.as_strided(size=(x.shape[0], n, w, *list(x.shape[4:])), stride=new_stride,
-                       storage_offset= start * stride[1] + (end) * stride[2] + headstart * stride[3])
+    return x.as_strided(
+        size=(x.shape[0], n, w, *list(x.shape[4:])),
+        stride=new_stride,
+        storage_offset=start * stride[1]
+        + (end) * stride[2]
+        + headstart * stride[3],
+    )
+
 
 def stripe_need_dad_add_(x, y, n, w, start, end, headstart):
     x = x.contiguous()
@@ -314,7 +390,17 @@ def stripe_need_dad_add_(x, y, n, w, start, end, headstart):
     new_stride.append(stride[1] + stride[2] + stride[3])
     new_stride.append(stride[3])
     new_stride.extend(stride[4:])
-    tmp = x.as_strided(size=(x.shape[0], n, w, *list(x.shape[4:])), stride=new_stride,
-                       storage_offset=start * stride[1] + (end) * stride[2] + headstart * stride[3])
-    x.as_strided(size=(x.shape[0], n, w, *list(x.shape[4:])), stride=new_stride,
-                 storage_offset=start * stride[1] + (end) * stride[2] + headstart * stride[3]).copy_(tmp + y)
+    tmp = x.as_strided(
+        size=(x.shape[0], n, w, *list(x.shape[4:])),
+        stride=new_stride,
+        storage_offset=start * stride[1]
+        + (end) * stride[2]
+        + headstart * stride[3],
+    )
+    x.as_strided(
+        size=(x.shape[0], n, w, *list(x.shape[4:])),
+        stride=new_stride,
+        storage_offset=start * stride[1]
+        + (end) * stride[2]
+        + headstart * stride[3],
+    ).copy_(tmp + y)
